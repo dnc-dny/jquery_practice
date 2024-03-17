2d8ce3210a3d2aa7e4670e76290b596fd178a8d0
山崎早和子 <16ic035e@gmail.jp>
2024-03-17 23:02:08 +0900

    first push

--- q2/work/js/app.js
+++ q2/work/js/app.js
@@ -1,8 +1,12 @@
 $(function(){
+  // modal_open_buttonセレクタにクリックイベントを用いる
   $('.modal_open_button').click(function(){
+    // modal_winセレクタにfadeInメソッドを指定→modal_open_buttonをクリックしたら、modal_winがフェードイン
     $('.modal_win').fadeIn();
   });
+  // modal_close_buttonセレクタにクリックイベントを用いる
   $('.modal_close_button').click(function(){
+    // modal_winセレクタにfadeOutメソッドを指定→modal_close_buttonをクリックしたら、modal_winがフェードアウト
     $('.modal_win').fadeOut();
   });
 });

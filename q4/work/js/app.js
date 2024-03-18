fcc0decf55cbaf7022525d2ad483ecb48c559c1f
山崎早和子 <16ic035e@gmail.jp>
2024-03-18 22:39:01 +0900

    first push

--- q4/work/js/app.js
+++ q4/work/js/app.js
@@ -4,9 +4,9 @@ $(function(){
     // navクラスのliセレクタの、クリックした要素のインデックス番号を変数に代入
     const num = $('.nav li').index(this);
     // descriptionクラスのliセレクタに、is-hiddenクラスを追加して、表示されている要素を非表示にする
-    $('.description li').addClass('.is-hidden');
+    $('.description li').addClass('is-hidden');
     // descriptionクラスのliセレクタに、navクラスのliセレクタのクリックしたインデックス番号と
     // 同じインデックス番号からis-hiddenクラスを取り除くことで、表示させる
-    $('.description li').eq(num).removeClass('.is-hidden');
+    $('.description li').eq(num).removeClass('is-hidden');
   });
 });

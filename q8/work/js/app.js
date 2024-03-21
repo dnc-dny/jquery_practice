b66e1655f47eb6f8dab9e6e138711bfd8055c49b
山崎早和子 <16ic035e@gmail.jp>
2024-03-21 22:41:25 +0900

    second push

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -20,7 +20,7 @@ $(function(){
     0 < (null == (response = event[0].items) ? void 0 : response.length) ?
     // 上記一致する場合は入力した値を対象に繰り返し処理を実行
     $.each(event[0].items, function (h, c) {
-      // HTMLを追加し、変数に代入
+      // HTMLを追加し、定数に代入
       const text = '<li class="lists-item"><div class="list-inner"><p>タイトル：' +
       // タイトルが一致ならそのタイトルを、一致しなかったら文言を追加
       ((c.title ? c.title : "タイトルなし") + "</p><p>作者：") +
@@ -36,13 +36,13 @@ $(function(){
     // 一致しない場合は、listsクラスの前に文言を追加
     : $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>')
   }
-  // ページ数の初期値が1 の情報を変数に代入
+  // ページ数の初期値が1 の情報を定数に代入
   const pageCount = 1;
-  // 検索結果に表示される書籍 の情報を空にして変数に代入
+  // 検索結果に表示される書籍 の情報を空にして定数に代入
   const book = "";
   // 検索ボタンにonメソッドを用いてclickイベントを指定する
   $(".search-btn").on("click", function () {
-    // 検索ボックスに入力した値を取得し、変数に代入
+    // 検索ボックスに入力した値を取得し、定数に代入
     const searchWord = $("#search-input").val();
     // 検索ボックスに入力した値と書籍の情報が一致しなかった場合は、ページ数は1、listsの子要素を削除
     searchWord !== book ? (pageCount = 1, $(".lists").empty(), book = e) :

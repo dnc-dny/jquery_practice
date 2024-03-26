7f65d807dc92d4349684ac1fc2375f02d79f5412
山崎早和子 <16ic035e@gmail.jp>
2024-03-26 21:27:09 +0900

    third push

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -7,16 +7,18 @@ $(function(){
     // food-listクラスの子要素を、定数に代入
     const list = $('.food-list li');
     // value属性がallの場合はfood-listクラスの子要素を全て表示する
-    'all' === value ? list.show() :
-    // value属性がallで無い場合に、food-listクラスの子要素を対象にeachメソッドで繰り返し処理を実行する
-    // e→イベントオブジェクト（クリックした要素を取得）、text→food-listクラスの子要素のテキスト
-    $.each(list, function(e, text){
-      // food-listクラスの子要素のテキストの属性を、定数に代入
-      const type = $(text).data('category-type');
-      // クリックしたvalue属性とfood-listクラスの子要素テキストの属性が一致した場合はテキストを表示
-      value === type ? $(text).show() :
-      // 一致しなかった場合はテキストを隠す
-      $(text).hide();
-    });
+    if('all' === value) {
+      list.show();
+    } else {
+      // value属性がallで無い場合に、food-listクラスの子要素を対象にeachメソッドで繰り返し処理を実行する
+      // e→イベントオブジェクト（クリックした要素を取得）、text→food-listクラスの子要素のテキスト
+      $.each(list, function(e, text){
+        // food-listクラスの子要素のテキストの属性を、定数に代入
+        const type = $(text).data('category-type');
+        // クリックしたvalue属性とfood-listクラスの子要素テキストの属性が一致した場合はテキストを表示
+        // 一致しなかった場合はテキストを隠す
+        value === type ? $(text).show() : $(text).hide();
+      });
+    };
   });
 });
